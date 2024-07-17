3edd8d3b401c6f5bf804b28ca95cbfc6d1b14985
simondumalski <simon@dumalski.com>
2024-07-16 23:41:58 -0600

    Remove constructor and change init

--- src/main/java/me/simondumalski/betterlib/BetterLib.java
+++ src/main/java/me/simondumalski/betterlib/BetterLib.java
@@ -1,13 +1,18 @@
 package me.simondumalski.betterlib;
 
+import fr.minuskube.inv.InventoryManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterLib {
+
+    public static InventoryManager inventoryManager;
 
     public static JavaPlugin plugin;
 
     public static void init(JavaPlugin plugin) {
         BetterLib.plugin = plugin;
+        inventoryManager = new InventoryManager(plugin);
+        inventoryManager.init();
     }
 
 }
--- src/main/java/me/simondumalski/betterlib/BetterPlugin.java
+++ src/main/java/me/simondumalski/betterlib/BetterPlugin.java
@@ -1,25 +1,8 @@
 package me.simondumalski.betterlib;
 
-import fr.minuskube.inv.InventoryManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class BetterPlugin extends JavaPlugin {
-
-    /**
-     * SmartInvs API inventory manager
-     */
-    private final InventoryManager inventoryManager;
-
-    /**
-     * Default constructor for the better plugin class
-     */
-    public BetterPlugin(JavaPlugin plugin) {
-        super();
-        BetterLib.init(plugin);
-        inventoryManager = new InventoryManager(BetterLib.plugin);
-        inventoryManager.init();
-        System.out.println("TEST TEST TEST");
-    }
 
     /**
      * Registers the plugin event listeners
@@ -40,13 +23,5 @@ public abstract class BetterPlugin extends JavaPlugin {
      * Reloads the config.yml and other plugin functions
      */
     public abstract void reload();
-
-    /**
-     * Gets the SmartInvs API inventory manager
-     * @return Inventory manager
-     */
-    public final InventoryManager getInventoryManager() {
-        return inventoryManager;
-    }
 
 }
